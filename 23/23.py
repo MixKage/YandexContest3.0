617a38357719a98d85e5f3fefe1029610935a111
Valerij <55548743+MixKage@users.noreply.github.com>
2023-02-28 01:23:29 +0300

    Create 23.py

---
+++ 23/23.py
@@ -0,0 +1,17 @@
+subsequence = [1]
+
+def f(n):
+    cache = [0] * (n + 1)
+    for i in range(2, n + 1):
+        v = cache[i - 1]
+        if i % 2 == 0:
+            v = min(v, cache[i // 2])
+        if i % 3 == 0:
+            v = min(v, cache[i // 3])
+        cache[i] = v + 1
+        if(cache[i-1]<cache[i]):
+            subsequence.append(i)
+    return cache[-1]
+
+print(f(int(input())))
+print(*subsequence, sep = ' ')
